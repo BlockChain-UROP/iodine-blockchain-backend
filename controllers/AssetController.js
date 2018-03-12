962a3dc63c3cda9227b3aa7c705c3bd06185560e
Hu Yao-Chieh <yhuag@connect.ust.hk>
2018-03-12 19:40:28 +0800

    solve createClient by removing gas and add HDWallet

--- controllers/AssetController.js
+++ controllers/AssetController.js
@@ -1,7 +1,15 @@
 // Libraries
 var Web3 = require('web3');
 
-var web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/lMgQfS5DDh31T0z6iD5E'));
+// var web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.ifura.io/lMgQfS5DDh31T0z6iD5E'));
+// var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+
+
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "force pistol endless treat spot craft easily panel hurt potato slide explain";
+var provider = new HDWalletProvider(mnemonic, "https://ropsten.infura.io/lMgQfS5DDh31T0z6iD5E");
+var web3 = new Web3(provider);
+
 
 var TruffleContract = require("truffle-contract");
 
@@ -23,24 +31,33 @@ if (typeof Custodian.currentProvider.sendAsync !== "function") {
 
 // Initialization
 // var accounts;
-var CUSTODIAN_CONTRACT_ADDRESS = "0x71e923ee401e9604d3ae9cd0945b91bc613a6708";
+var CUSTODIAN_CONTRACT_ADDRESS = "0xFDe186Ddb09ef7b83FD997CC5a7461E8E8af56C9";
+var test_account = "0x5ff2c17ada131e5d9fa0f927395abe35657e4768";
 
 
-var test_account = "0x9BDe18763610E7beEE45F522B641F156D538d901";
 
 exports.info = async function(req, res) {
 
     try {
+        // let accounts = [];
+        web3.eth.getAccounts((err, res) => {
+            console.log(res);
+        });
+        // console.log(accounts);
+        // await web3.personal.unlockAccount(test_account, "Pass0970");
+
+
         // Get Seed
         var instance = await Custodian.at(CUSTODIAN_CONTRACT_ADDRESS);
-        console.log(instance);
+        console.log("instance");
         var seed = await instance.getSeed({from: test_account});
+        console.log("seed", seed);
 
         var client = await instance.createClient({from: test_account});
         console.log(client);
 
         var volume = await instance.volume.call({from: test_account});
-        console.log(volume);
+        console.log("volume", volume);
 
     } catch (error) {
         console.error(error);
